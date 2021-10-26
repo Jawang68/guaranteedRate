64263b8516215cc1d28b5261e817be0893e3ffde
James.Wang <jawang68@gmail.com>
2021-10-26 13:54:32 -0700

    Create skeleton project for the commandLine app

---
+++ RecordProcesssor/Model/PersonRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordProcesssor.Model
+{
+    public class PersonRecord
+    {
+        public String LastName { get; set;}
+        public String FirstName { get; set;}
+        public String Email { get; set; }
+        public String FavoriteColor { get; set;}
+        public DateTime  DateOfBirth { get; set;}
+    }
+}
