3d508fb0f306658bf4528bb31833f90fdc47a330
James.Wang <jawang68@gmail.com>
2021-10-27 14:23:23 -0700

    Add the skeleton project for the WebApi

---
+++ GRWebApi/Controllers/GRWebApiController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RecordProcesssor.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GRWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]/records/")]
+
+    public class GRWebApiController : ControllerBase
+    {
+        public GRWebApiController()
+        {
+        }
+
+        [HttpGet()]
+        public IActionResult Authors()
+        {
+            return new JsonResult("authors");
+        }
+
+        [HttpPost]
+        public IActionResult SaveRecords(PersonRecord record)
+        {
+            return Ok();
+        }
+
+
+        [HttpGet("{orderBy}")]
+        public IActionResult GetRecords(string orderBy)
+        {
+            return new JsonResult(orderBy);
+        }
+    }
+}
